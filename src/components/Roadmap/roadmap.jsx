061f8199d724d08e6f22461e637662353765a6b2
mamorshedalam <ma.morshedalampro@gmail.com>
2023-06-21 11:08:32 +0800

    roadmap sectiom completd

--- src/components/Roadmap/roadmap.jsx
+++ src/components/Roadmap/roadmap.jsx
@@ -7,29 +7,75 @@ export default function Roadmap() {
                <div className="sl-container">
                     <h2 className="font-semibold lg:text-5xl md:text-3xl text-2xl text-center tracking-wider lg:mb-6 sm:mb-4 mb-2">Road<span className="text-sky-400">map</span></h2>
                     <p className="text-center mb-6">At EAGLE-X, we are committed to driving the adoption of blockchain technology for social impact. Our roadmap outlines the key milestones on our journey towards creating positive change and empowering communities.</p>
-                    {/* <ul className="text-center space-y-2">
-                         <li>Project Conceptualization and Team Formation: <span className="font-semibold">Q3 2023</span></li>
-                         <li><svg className="fill-sky-400 mx-auto h-6" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><path d="M201.4 342.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 274.7 86.6 137.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"/></svg></li>
-                         <li>Research and Development (Phase 1):  <span className="font-semibold">Q3 2023 </span></li>
-                         <li><svg className="fill-sky-400 mx-auto h-6" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><path d="M201.4 342.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 274.7 86.6 137.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"/></svg></li>
-                         <li>Token Development and Smart Contract Creation:  <span className="font-semibold">Q4 2023</span></li>
-                         <li><svg className="fill-sky-400 mx-auto h-6" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><path d="M201.4 342.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 274.7 86.6 137.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"/></svg></li>
-                         <li>Marketing and Community Building (Phase 1):  <span className="font-semibold">Q3 and Q4 2023</span></li>
-                         <li><svg className="fill-sky-400 mx-auto h-6" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><path d="M201.4 342.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 274.7 86.6 137.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"/></svg></li>
-                         <li>Initial DEX Offering (Presale & Public Sale Series):  <span className="font-semibold">Q4 2023 - Q1 2024</span></li>
-                         <li><svg className="fill-sky-400 mx-auto h-6" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><path d="M201.4 342.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 274.7 86.6 137.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"/></svg></li>
-                         <li>Launch and Listing on Exchanges:  <span className="font-semibold">Q1 2024 </span></li>
-                         <li><svg className="fill-sky-400 mx-auto h-6" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><path d="M201.4 342.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 274.7 86.6 137.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"/></svg></li>
-                         <li>Establishment of EAGLE-X Impact Foundation (Nigeria Pilot Plan):  <span className="font-semibold">Q2 2024</span></li>
-                         <li><svg className="fill-sky-400 mx-auto h-6" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><path d="M201.4 342.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 274.7 86.6 137.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"/></svg></li>
-                         <li>Expansion to Impact Projects:  <span className="font-semibold">Q3 2024</span></li>
-                         <li><svg className="fill-sky-400 mx-auto h-6" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><path d="M201.4 342.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 274.7 86.6 137.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"/></svg></li>
-                         <li>Ecosystem Development and Strategic Partnerships (Phase 2):  <span className="font-semibold">Q4 2024 - Q1 2025</span></li>
-                         <li><svg className="fill-sky-400 mx-auto h-6" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><path d="M201.4 342.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 274.7 86.6 137.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"/></svg></li>
-                         <li>Research and Development (Phase 2):  <span className="font-semibold">Q3 2025</span></li>
-                         <li><svg className="fill-sky-400 mx-auto h-6" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><path d="M201.4 342.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 274.7 86.6 137.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"/></svg></li>
-                         <li>Africa Implementation Plan:  <span className="font-semibold">Q4 2025 - Q1 2026</span></li>
-                    </ul> */}
+                    <div className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-1 pb-9">
+                         <div className="relative block px-4 overflow-hidden mb-6 before:absolute md:before:top-16 before:top-[2.7rem] before:left-0 before:h-2 before:w-full before:bg-sky-400/10">
+                              <h3 className="font-semibold text-sky-400 md:mb-10 mb-6">Q3 2023</h3>
+                              <div className="relative h-full md:ml-20 ml-5 md:pl-6 pl-4 pb-2 md:pt-5 pt-3 border-l-2 border-sky-400 before:absolute before:-left-4 before:-top-4 before:w-8 before:h-8 before:rounded-full before:bg-sky-400/20  after:absolute after:-left-2 after:-top-2 after:w-4 after:h-4 after:rounded-full after:bg-sky-400">
+                                   <p className="text-base md:pt-6 pt-4 text-gray-300">Project Conceptualization and Team Formation</p>
+                                   <p className="text-base md:pt-6 pt-4 text-gray-300">Research and Development (Phase 1)</p>
+                              </div>
+                         </div>
+                         <div className="relative block px-4 overflow-hidden mb-6 before:absolute md:before:top-16 before:top-[2.7rem] before:left-0 before:h-2 before:w-full before:bg-sky-400/10">
+                              <h3 className="font-semibold text-amber-500 md:mb-10 mb-6">Q3 2023</h3>
+                              <div className="relative h-full md:ml-20 ml-5 md:pl-6 pl-4 pb-2 md:pt-5 pt-3 border-l-2 border-amber-500 before:absolute before:-left-4 before:-top-4 before:w-8 before:h-8 before:rounded-full before:bg-amber-500/20  after:absolute after:-left-2 after:-top-2 after:w-4 after:h-4 after:rounded-full after:bg-amber-500">
+                                   <p className="text-base md:pt-6 pt-4 text-gray-300">Research and Development (Phase 1)</p>
+                              </div>
+                         </div>
+                         <div className="relative block px-4 overflow-hidden mb-6 before:absolute md:before:top-16 before:top-[2.7rem] before:left-0 before:h-2 before:w-full before:bg-sky-400/10">
+                              <h3 className="font-semibold text-pink-500 md:mb-10 mb-6">Q4 2023</h3>
+                              <div className="relative h-full md:ml-20 ml-5 md:pl-6 pl-4 pb-2 md:pt-2 pt-30 border-l-2 border-pink-500 before:absolute before:-left-4 before:-top-4 before:w-8 before:h-8 before:rounded-full before:bg-pink-500/20  after:absolute after:-left-2 after:-top-2 after:w-4 after:h-4 after:rounded-full after:bg-pink-500">
+                                   <p className="text-base md:pt-6 pt-4 text-gray-300">Token Development and Smart Contract Creation</p>
+                              </div>
+                         </div>
+                         <div className="relative block px-4 overflow-hidden mb-6 before:absolute md:before:top-16 before:top-[2.7rem] before:left-0 before:h-2 before:w-full before:bg-sky-400/10">
+                              <h3 className="font-semibold text-blue-500 md:mb-10 mb-6">Q3 and Q4 2023</h3>
+                              <div className="relative h-full md:ml-20 ml-5 md:pl-6 pl-4 pb-2 md:pt-2 pt-30 border-l-2 border-blue-500 before:absolute before:-left-4 before:-top-4 before:w-8 before:h-8 before:rounded-full before:bg-blue-500/20  after:absolute after:-left-2 after:-top-2 after:w-4 after:h-4 after:rounded-full after:bg-blue-500">
+                                   <p className="text-base md:pt-6 pt-4 text-gray-300">Marketing and Community Building (Phase 1)</p>
+                              </div>
+                         </div>
+                         <div className="relative block px-4 overflow-hidden mb-6 before:absolute md:before:top-16 before:top-[2.7rem] before:left-0 before:h-2 before:w-full before:bg-sky-400/10">
+                              <h3 className="font-semibold text-sky-400 md:mb-10 mb-6">Q4 2023 - Q1 2024</h3>
+                              <div className="relative h-full md:ml-20 ml-5 md:pl-6 pl-4 pb-2 md:pt-2 pt-30 border-l-2 border-sky-400 before:absolute before:-left-4 before:-top-4 before:w-8 before:h-8 before:rounded-full before:bg-sky-400/20  after:absolute after:-left-2 after:-top-2 after:w-4 after:h-4 after:rounded-full after:bg-sky-400">
+                                   <p className="text-base md:pt-6 pt-4 text-gray-300">Initial DEX Offering (Presale & Public Sale Series)</p>
+                              </div>
+                         </div>
+                         <div className="relative block px-4 overflow-hidden mb-6 before:absolute md:before:top-16 before:top-[2.7rem] before:left-0 before:h-2 before:w-full before:bg-sky-400/10">
+                              <h3 className="font-semibold text-amber-500 md:mb-10 mb-6">Q2 2024</h3>
+                              <div className="relative h-full md:ml-20 ml-5 md:pl-6 pl-4 pb-2 md:pt-2 pt-30 border-l-2 border-amber-500 before:absolute before:-left-4 before:-top-4 before:w-8 before:h-8 before:rounded-full before:bg-amber-500/20  after:absolute after:-left-2 after:-top-2 after:w-4 after:h-4 after:rounded-full after:bg-amber-500">
+                                   <p className="text-base md:pt-6 pt-4 text-gray-300">Launch and Listing on Exchanges</p>
+                              </div>
+                         </div>
+                         <div className="relative block px-4 overflow-hidden mb-6 before:absolute md:before:top-16 before:top-[2.7rem] before:left-0 before:h-2 before:w-full before:bg-sky-400/10">
+                              <h3 className="font-semibold text-pink-500 md:mb-10 mb-6">Q4 2024 - Q1 2025</h3>
+                              <div className="relative h-full md:ml-20 ml-5 md:pl-6 pl-4 pb-2 md:pt-2 pt-30 border-l-2 border-pink-500 before:absolute before:-left-4 before:-top-4 before:w-8 before:h-8 before:rounded-full before:bg-pink-500/20  after:absolute after:-left-2 after:-top-2 after:w-4 after:h-4 after:rounded-full after:bg-pink-500">
+                                   <p className="text-base md:pt-6 pt-4 text-gray-300">Establishment of EAGLE-X Impact Foundation (Nigeria Pilot Plan)</p>
+                              </div>
+                         </div>
+                         <div className="relative block px-4 overflow-hidden mb-6 before:absolute md:before:top-16 before:top-[2.7rem] before:left-0 before:h-2 before:w-full before:bg-sky-400/10">
+                              <h3 className="font-semibold text-blue-500 md:mb-10 mb-6">Q3 2025</h3>
+                              <div className="relative h-full md:ml-20 ml-5 md:pl-6 pl-4 pb-2 md:pt-2 pt-30 border-l-2 border-blue-500 before:absolute before:-left-4 before:-top-4 before:w-8 before:h-8 before:rounded-full before:bg-blue-500/20  after:absolute after:-left-2 after:-top-2 after:w-4 after:h-4 after:rounded-full after:bg-blue-500">
+                                   <p className="text-base md:pt-6 pt-4 text-gray-300">Expansion to Impact Projects</p>
+                              </div>
+                         </div>
+                         <div className="relative block px-4 overflow-hidden mb-6 before:absolute md:before:top-16 before:top-[2.7rem] before:left-0 before:h-2 before:w-full before:bg-sky-400/10">
+                              <h3 className="font-semibold text-sky-400 md:mb-10 mb-6">Q4 2025 - Q1 2026</h3>
+                              <div className="relative h-full md:ml-20 ml-5 md:pl-6 pl-4 pb-2 md:pt-2 pt-30 border-l-2 border-sky-400 before:absolute before:-left-4 before:-top-4 before:w-8 before:h-8 before:rounded-full before:bg-sky-400/20  after:absolute after:-left-2 after:-top-2 after:w-4 after:h-4 after:rounded-full after:bg-sky-400">
+                                   <p className="text-base md:pt-6 pt-4 text-gray-300">Ecosystem Development and Strategic Partnerships (Phase 2)</p>
+                              </div>
+                         </div>
+                         <div className="relative block px-4 overflow-hidden mb-6 before:absolute md:before:top-16 before:top-[2.7rem] before:left-0 before:h-2 before:w-full before:bg-sky-400/10">
+                              <h3 className="font-semibold text-amber-500 md:mb-10 mb-6">Q3 2025</h3>
+                              <div className="relative h-full md:ml-20 ml-5 md:pl-6 pl-4 pb-2 md:pt-2 pt-30 border-l-2 border-amber-500 before:absolute before:-left-4 before:-top-4 before:w-8 before:h-8 before:rounded-full before:bg-amber-500/20  after:absolute after:-left-2 after:-top-2 after:w-4 after:h-4 after:rounded-full after:bg-amber-500">
+                                   <p className="text-base md:pt-6 pt-4 text-gray-300">Research and Development (Phase 2)</p>
+                              </div>
+                         </div>
+                         <div className="relative block px-4 overflow-hidden mb-6 before:absolute md:before:top-16 before:top-[2.7rem] before:left-0 before:h-2 before:w-full before:bg-sky-400/10">
+                              <h3 className="font-semibold text-pink-500 md:mb-10 mb-6">Q4 2025 - Q1 2026</h3>
+                              <div className="relative h-full md:ml-20 ml-5 md:pl-6 pl-4 pb-2 md:pt-2 pt-30 border-l-2 border-pink-500 before:absolute before:-left-4 before:-top-4 before:w-8 before:h-8 before:rounded-full before:bg-pink-500/20  after:absolute after:-left-2 after:-top-2 after:w-4 after:h-4 after:rounded-full after:bg-pink-500">
+                                   <p className="text-base md:pt-6 pt-4 text-gray-300">Africa Implementation Plan</p>
+                              </div>
+                         </div>
+                    </div>
                     <p className="text-center mt-9">Through our roadmap, we demonstrate our commitment to transparency, innovation, and creating sustainable social change. Each milestone brings us closer to our vision of leveraging blockchain technology to empower communities and foster positive social impact.</p>
                </div>
           </section>
