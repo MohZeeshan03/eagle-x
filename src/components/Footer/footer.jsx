e1b0d29cbe19f174b905bf3bbbdbcdab0f183b64
mamorshedalam <ma.morshedalampro@gmail.com>
2023-06-22 14:42:31 +0800

    all revision completed

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -2,19 +2,28 @@ import logo from "../../assets/img/logo.png"
 
 export default function Footer() {
 
+     const scrollTopSmooth = () => {
+          window.scrollTo({
+               top: 0,
+               left: 0,
+               behavior: "smooth",
+          })
+     }
+
      return (
           <footer className="bg-gradient-to-b from-gray-900 to-gray-950 text-gray-400 text-base lg:py-10 md:pb-9 pb-4">
                <div className="sl-container">
-                    <div className="flex flex-wrap items-start justify-between lg:py-14 md:py-9 py-6 border-t border-sky-200">
-                         <div className="lg:w-1/4 md:w-1/2 w-full px-4 mb-4 md:space-y-6 space-y-4 md:text-left text-center">
+                    <button onClick={scrollTopSmooth} className="absolute cursor-pointer -top-6 left-1/2 -translate-x-1/2 w-12 h-12 rounded-full bg-gray-700 z-50"><svg className="fill-sky-200 w-6 mx-auto hover:fill-sky-400 sl-animated-lg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M201.4 137.4c12.5-12.5 32.8-12.5 45.3 0l160 160c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L224 205.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l160-160z" /></svg></button>
+                    <div className="flex flex-wrap items-start justify-between lg:py-14 md:py-9 py-6 border-t border-gray-700">
+                         <div className="md:order-1 order-1 lg:w-1/4 md:w-2/5 w-full px-4 mb-4 md:space-y-6 space-y-4 md:text-left text-center">
                               <img className="w-20 md:mx-0 mx-auto" src={logo} alt="logo" />
                               <p>Empowering Communities Through Blockchain Technology</p>
                               <ul className="flex flex-row items-center md:justify-start justify-center space-x-5">
-                                   <li><a href="mailto:eagleximpact@gmail.com" className="flex items-center justify-center w-10 h-10 rounded-full border-2 border-sky-200 hover:bg-sky-400 hover:border-sky-400 sl-animated-xl"><svg className="fill-white w-4" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"><path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/></svg></a></li>
+                                   <li><a href="mailto:eagleximpact@gmail.com" className="flex items-center justify-center w-10 h-10 rounded-full border-2 border-sky-200 hover:bg-sky-400 hover:border-sky-400 sl-animated-xl"><svg className="fill-white w-4" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"><path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z" /></svg></a></li>
                                    <li><a href="https://twitter.com/EaglexImpact" className="flex items-center justify-center w-10 h-10 rounded-full border-2 border-sky-200 hover:bg-sky-400 hover:border-sky-400 sl-animated-xl"><svg className="fill-white w-4" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z" /></svg></a></li>
                               </ul>
                          </div>
-                         <div className="lg:w-1/4 w-1/2 px-4 mb-4">
+                         <div className="md:order-2 order-3 lg:w-1/4 md:w-1/5 w-full px-4 mb-4">
                               <h2 className="text-white lg:text-xl md:text-lg text-base uppercase font-semibold lg:my-6 md:my-4 my-2">useful links</h2>
                               <ul className="space-y-2">
                                    <li><a href="#contact" className="hover:text-sky-400 sl-animated-lg">Contact us</a></li>
@@ -23,7 +32,7 @@ export default function Footer() {
                                    <li><a href="/public/Data-Privacy-terms.pdf" className="hover:text-sky-400 sl-animated-lg">Terms & Services</a></li>
                               </ul>
                          </div>
-                         <div className="lg:w-1/4 md:w-1/2 w-full px-4 mb-4">
+                         <div className="md:order-3 order-2 lg:w-1/4 md:w-2/5 w-full px-4 mb-4">
                               <h2 className="text-white lg:text-xl md:text-lg text-base uppercase font-semibold lg:my-6 md:my-4 my-2">subscribe newsletter</h2>
                               <p>Submit your Email for our NewsLetter</p>
                               <form action="">
--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import logo from "../../assets/img/logo.png"
 
 export default function header() {
@@ -10,6 +10,33 @@ export default function header() {
                .scrollIntoView({ behavior: "smooth" })
      }
 
+     useEffect(() => {
+          const mainNavLinks = document.querySelectorAll("nav ul li a");
+
+          const handleScroll = () => {
+               let fromTop = window.scrollY;
+
+               mainNavLinks.forEach(link => {
+                    let section = document.querySelector(link.hash);
+
+                    if (
+                         section.offsetTop <= fromTop + 160 &&
+                         section.offsetTop + section.offsetHeight - 160 > fromTop
+                    ) {
+                         link.classList.add("after:w-full");
+                    } else {
+                         link.classList.remove("after:w-full");
+                    }
+               });
+          };
+
+          window.addEventListener('scroll', handleScroll);
+
+          return () => {
+               window.removeEventListener('scroll', handleScroll);
+          };
+     }, []);
+
      return (
           <header id="header" className="fixed lg:text-sm text-xs bg-gray-950 w-full z-50">
                <div className="flex items-center w-full justify-between lg:space-x-5 border-b-2 border-gray-700 xl:px-9 px-4">
@@ -17,13 +44,13 @@ export default function header() {
                     <button className="lg:hidden block" onClick={() => sidebar ? setSidebar(false) : setSidebar(true)}><svg className="fill-white hover:fill-sky-400 w-6 sl-animated-lg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" /></svg></button>
                     <nav className="lg:block hidden">
                          <ul className="flex items-center xl:space-x-12 lg:space-x-6 space-x-4 font-semibold">
-                              <li><button onClick={() => smoothScroll("hero")} className="relative h-full px-1 py-8 uppercase after:absolute after:bottom-0 after:left-0 after:w-full after:h-1 after:bg-sky-400">home</button></li>
-                              <li><button onClick={() => smoothScroll("about")} className="relative h-full px-1 py-8 uppercase after:absolute after:bottom-0 after:left-0 after:w-0 after:h-1 after:bg-sky-400 hover:after:w-full sl-animated-xl">about us</button></li>
-                              <li><button onClick={() => smoothScroll("tokenomics")} className="relative h-full px-1 py-8 uppercase after:absolute after:bottom-0 after:left-0 after:w-0 after:h-1 after:bg-sky-400 hover:after:w-full sl-animated-xl">tokenomics</button></li>
-                              <li><button onClick={() => smoothScroll("roadmap")} className="relative h-full px-1 py-8 uppercase after:absolute after:bottom-0 after:left-0 after:w-0 after:h-1 after:bg-sky-400 hover:after:w-full sl-animated-xl">roadmap</button></li>
-                              <li><button onClick={() => smoothScroll("ecosystem")} className="relative h-full px-1 py-8 uppercase after:absolute after:bottom-0 after:left-0 after:w-0 after:h-1 after:bg-sky-400 hover:after:w-full sl-animated-xl">ecosystem</button></li>
-                              <li><button onClick={() => smoothScroll("team")} className="relative h-full px-1 py-8 uppercase after:absolute after:bottom-0 after:left-0 after:w-0 after:h-1 after:bg-sky-400 hover:after:w-full sl-animated-xl">team</button></li>
-                              <li><button onClick={() => smoothScroll("contact")} className="relative h-full px-1 py-8 uppercase after:absolute after:bottom-0 after:left-0 after:w-0 after:h-1 after:bg-sky-400 hover:after:w-full sl-animated-xl">contact us</button></li>
+                              <li><a href="#hero" onClick={() => smoothScroll("hero")} className="relative h-full px-1 py-8 uppercase after:absolute after:bottom-0 after:left-0 after:w-full after:h-1 after:bg-sky-400 hover:after:w-full sl-animated-xl">home</a></li>
+                              <li><a href="#about" onClick={() => smoothScroll("about")} className="relative h-full px-1 py-8 uppercase after:absolute after:bottom-0 after:left-0 after:w-0 after:h-1 after:bg-sky-400 hover:after:w-full sl-animated-xl">about us</a></li>
+                              <li><a href="#tokenomics" onClick={() => smoothScroll("tokenomics")} className="relative h-full px-1 py-8 uppercase after:absolute after:bottom-0 after:left-0 after:w-0 after:h-1 after:bg-sky-400 hover:after:w-full sl-animated-xl">tokenomics</a></li>
+                              <li><a href="#roadmap" onClick={() => smoothScroll("roadmap")} className="relative h-full px-1 py-8 uppercase after:absolute after:bottom-0 after:left-0 after:w-0 after:h-1 after:bg-sky-400 hover:after:w-full sl-animated-xl">roadmap</a></li>
+                              <li><a href="#ecosystem" onClick={() => smoothScroll("ecosystem")} className="relative h-full px-1 py-8 uppercase after:absolute after:bottom-0 after:left-0 after:w-0 after:h-1 after:bg-sky-400 hover:after:w-full sl-animated-xl">ecosystem</a></li>
+                              <li><a href="#team" onClick={() => smoothScroll("team")} className="relative h-full px-1 py-8 uppercase after:absolute after:bottom-0 after:left-0 after:w-0 after:h-1 after:bg-sky-400 hover:after:w-full sl-animated-xl">team</a></li>
+                              <li><a href="#contact" onClick={() => smoothScroll("contact")} className="relative h-full px-1 py-8 uppercase after:absolute after:bottom-0 after:left-0 after:w-0 after:h-1 after:bg-sky-400 hover:after:w-full sl-animated-xl">contact us</a></li>
                          </ul>
                     </nav>
                     <button type="button" className="lg:block hidden py-3 lg:px-10 px-6 border-2 border-sky-500 rounded-full uppercase font-semibold hover:border-sky-400 hover:text-sky-400 sl-animated-lg">Go to presale</button>
